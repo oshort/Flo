5f8e4d5ba17998893abb2ce13ef801c89eb606b7
Oliver Short <oshort55@gmail.com>
2016-12-02 14:51:13 -0500

    Added outline counter

--- Flo/CounterView.swift
+++ Flo/CounterView.swift
@@ -18,13 +18,10 @@ let π: CGFloat = CGFloat(M_PI)
     @IBInspectable var counterColor : UIColor = UIColor.orange
     
     override func draw(_ rect: CGRect) {
+    //Necessary parameters for creating an arc UIBezierPath
         let center = CGPoint(x:bounds.width/2, y: bounds.height/2)
-        
         let radius: CGFloat = max(bounds.width, bounds.height)
-
         let arcWidth: CGFloat = 76
-        
-
         let startAngle: CGFloat = 3 * π / 4
         let endAngle: CGFloat = π / 4
         
@@ -38,6 +35,39 @@ let π: CGFloat = CGFloat(M_PI)
         path.lineWidth = arcWidth
         counterColor.setStroke()
         path.stroke()
+        
+        //Draw the outline
+        
+        //1 - first calculate the difference between the two angles
+        //ensuring it is positive
+        let angleDifference: CGFloat = 2 * π - startAngle + endAngle
+        
+        //then calculate the arc for each single glass
+        let arcLengthPerGlass = angleDifference / CGFloat(numOfGlasses)
+        
+        //then multiply out by the actual glasses drunk
+        let outlineEndAngle = arcLengthPerGlass * CGFloat(counter) + startAngle
+        
+        //2 - draw the outer arc
+        var outlinePath = UIBezierPath(arcCenter: center,
+                                       radius: bounds.width/2 - 2.5,
+                                       startAngle: startAngle,
+                                       endAngle: outlineEndAngle,
+                                       clockwise: true)
+        
+        //3 - draw the inner arc
+        outlinePath.addArc(withCenter: center,
+                                     radius: bounds.width/2 - arcWidth + 2.5,
+                                     startAngle: outlineEndAngle,
+                                     endAngle: startAngle,
+                                     clockwise: false)
+        
+        //4 - close the path
+        outlinePath.close()
+        
+        outlineColor.setStroke()
+        outlinePath.lineWidth = 5.0
+        outlinePath.stroke()
     }
 
 }
